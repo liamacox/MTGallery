5e8636115d464f94a8e741e10a43138ff3740e14
Liam Cox <liam@liamc.ca>
2026-03-16 20:46:30 -0400

    Create initial scryfall api client

--- MtGambling/Program.cs
+++ MtGambling/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using MtGambling;
+
+var client = new ScryfallApiClient(@"C:\Users\Liam Cox\git\MtGambling\SetData");
+
+client.GetSetData("ecl");
+client.GetSetData("blb");
---
+++ MtGambling/ScryfallApiClient.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace MtGambling;
+
+public class ScryfallApiClient
+{
+    private readonly Dictionary<string, JsonArray> _setDataBySetCode = new();
+    private readonly string _dataDirectory;
+    
+    public ScryfallApiClient(string dataDirectory)
+    {
+        _dataDirectory = dataDirectory;
+        var files = Directory.GetFiles(dataDirectory);
+        foreach (var file in files)
+        {
+            var setCode = file.Remove(file.LastIndexOf('.'));
+            _setDataBySetCode.Add(setCode, new JsonArray(File.ReadAllText(file)));
+        }
+    }
+    
+    public JsonArray GetSetData(string setCode)
+    {
+        if (_setDataBySetCode.TryGetValue(setCode, out var jsonArray)) return jsonArray;
+        
+        jsonArray = [];
+        
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        client.DefaultRequestHeaders.Add("User-Agent", "MtGambling/1.0");
+
+        var queryString = $"https://api.scryfall.com/cards/search?order=rarity&q=set%3A{setCode}";
+        var response = client.GetAsync(queryString).Result;
+        var responseJson = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result);
+
+        AddCardDataToArray(jsonArray, responseJson.RootElement.GetProperty("data").EnumerateArray());
+        
+        while (responseJson.RootElement.GetProperty("has_more").GetBoolean())
+        {
+            response = client.GetAsync(responseJson.RootElement.GetProperty("next_page").GetString()).Result;
+            responseJson = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result);
+            AddCardDataToArray(jsonArray, responseJson.RootElement.GetProperty("data").EnumerateArray());
+        }
+        
+        var jsonOptions = new JsonSerializerOptions() { WriteIndented = true };
+        File.WriteAllText(
+            $"{_dataDirectory}\\{setCode}.json", 
+            jsonArray.ToJsonString(jsonOptions)
+            );
+        
+        return jsonArray;
+    }
+
+    private static void AddCardDataToArray(JsonArray jsonData, IEnumerable<JsonElement> cards)
+    {
+        foreach (var cardData in cards) jsonData.Add(cardData);
+    }
+}
