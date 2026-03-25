ffcdcff99b4aebaa263fbc5c48b7aa0a2a4bf531
liamacox <liam@liamc.ca>
2026-03-24 21:09:54 -0400

    make persistence work or something (#11)
    Closes #5

--- src/MtGambling/Program.cs
+++ src/MtGambling/Program.cs
@@ -1,11 +1,21 @@
 ﻿using MtGambling;
 using MtGambling.Packs;
+using Persistence;
 
 const string dataDirectory = @"C:\Users\Liam Cox\git\MtGambling\SetData";
+const string databasePath = @"C:\Users\Liam Cox\git\MtGambling\pulledCards.db";
 var client = new ScryfallApiClient(dataDirectory);
 var packGenerator = new PackGenerator(client, dataDirectory);
 
-foreach (var card in packGenerator.GeneratePack("ecl"))
+var repository = new SQLiteCardRepository(databasePath);
+await repository.InitializeAsync();
+
+foreach (var (card, count) in packGenerator.GeneratePack("ecl"))
 {
-    Console.WriteLine(card);
+    await repository.UpsertCardAsync(new CardDto(card.OracleId, count));
+}
+
+foreach (var cardDto in repository.GetCardsAsync().Result)
+{
+    Console.WriteLine(client.GetSetData("ecl").Single(card => card.OracleId == cardDto.OracleId));
 }
