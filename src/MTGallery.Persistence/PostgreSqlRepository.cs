cbc1f4252c3c910205e0bc861aa8f088e77ff6df
Liam Cox <liam@liamc.ca>
2026-03-27 13:37:53 -0400

    Sort and remove unecessary usings

--- src/MTGallery.Persistence/PostgreSqlRepository.cs
+++ src/MTGallery.Persistence/PostgreSqlRepository.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.Extensions.Caching.Memory;
 using MTGallery.Configuration;
 using MTGallery.Domain;
--- src/MTGallery/Program.cs
+++ src/MTGallery/Program.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
 using MTGallery;
 using MTGallery.Configuration;
 using MTGallery.Persistence;
-using Microsoft.Extensions.Caching.Memory;
 
 /* ---------------------------------------- CONFIGURATION ---------------------------------------- */
 
