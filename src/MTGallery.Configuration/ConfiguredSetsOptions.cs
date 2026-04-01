d98a55d3759c41ae6a93e71b9419717cd721a11b
Liam Cox <liam@liamc.ca>
2026-04-01 19:19:08 -0400

    Add AllConfiguredSets shortcut

--- src/MTGallery.Configuration/ConfiguredSetsOptions.cs
+++ src/MTGallery.Configuration/ConfiguredSetsOptions.cs
@@ -5,4 +5,6 @@ public class ConfiguredSetsOptions
     public required HashSet<string> ConfiguredSets { get; init; }
     public required HashSet<string> ConfiguredCommanderSets { get; init; }
     public required bool HydrateSetData { get; init; }
+    public HashSet<string> AllConfiguredSets => ConfiguredCommanderSets.Concat(ConfiguredSets).ToHashSet();
+
 }
