2122c247477ad9b5244a5491984866f30c13bc45
Liam Cox <liam@liamc.ca>
2026-04-06 18:20:20 -0400

    Add special guest configuration

--- src/MTGallery.Configuration/ConfiguredSetsOptions.cs
+++ src/MTGallery.Configuration/ConfiguredSetsOptions.cs
@@ -5,6 +5,8 @@ public class ConfiguredSetsOptions
     public required HashSet<string> ConfiguredSets { get; init; }
     public required HashSet<string> ConfiguredCommanderSets { get; init; }
     public required bool HydrateSetData { get; init; }
+    public required bool SpecialGuestsEnabled { get; init; }
+    public required HashSet<string> SpecialGuestsEnabledSets { get; init; }
+    public required Dictionary<string, string> SpecialGuestRangesBySet { get; init; } 
     public HashSet<string> AllConfiguredSets => ConfiguredCommanderSets.Concat(ConfiguredSets).ToHashSet();
-
 }
