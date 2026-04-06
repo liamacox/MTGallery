91c6c065608694657552942445bd23944254e9a7
Liam Cox <liam@liamc.ca>
2026-04-06 18:37:56 -0400

    Add api query to get special guest cards

--- src/MTGallery.Persistence/ScryfallApiClient.cs
+++ src/MTGallery.Persistence/ScryfallApiClient.cs
@@ -35,4 +35,34 @@ public static class ScryfallApiClient
         
         return cards;
     }
+
+    public static async Task<List<Card>> GetSpecialGuestDataAsync(string[] ranges, string setCode)
+    {
+        List<Card> cards = [];
+        
+        using HttpClient client = new();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        client.DefaultRequestHeaders.Add("User-Agent", "MTGallery/1.0");
+        
+        var queryString = $"https://api.scryfall.com/cards/search?q=set%3Aspg+cn>={ranges[0]}+cn<={ranges[1]}";
+        JsonDocument responseJson;
+        do
+        {
+            var response = await client.GetAsync(queryString);
+            responseJson = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+            if (!response.IsSuccessStatusCode) throw new ArgumentException(response.ReasonPhrase);
+
+            cards.AddRange(
+                responseJson.RootElement.GetProperty("data").EnumerateArray()
+                    .Where(jsonElement => jsonElement.GetProperty("games").ToString().Contains("paper"))
+                    .Select<JsonElement, Card>(card => Card.BuildSpecialGuestCardFromJson(card, setCode)));
+            
+            queryString = responseJson.RootElement.GetProperty("next_page").GetString();
+            Thread.Sleep(500);
+        } while (responseJson.RootElement.GetProperty("has_more").GetBoolean());
+        
+        return cards;
+    }
 }
