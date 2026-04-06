8623555e4895feb2b6297937ceca769761804aea
Liam Cox <liam@liamc.ca>
2026-04-06 18:37:32 -0400

    Add new special guest card builder from json

--- src/MTGallery.Domain/Card.cs
+++ src/MTGallery.Domain/Card.cs
@@ -31,6 +31,25 @@ public record Card(
             throw new ArgumentException($"Could not parse {cardJson.ToString()}");
         }
     }
+    
+    public static Card BuildSpecialGuestCardFromJson(JsonElement cardJson, string setCode)
+    {
+        try
+        {
+            var name = cardJson.GetProperty("name").GetString()!;
+            var rarity = Enum.Parse<Rarity>(cardJson.GetProperty("rarity").GetString()!, ignoreCase: true);
+            var oracleId = cardJson.GetProperty("oracle_id").GetString()!;
+            var scryfallUri = cardJson.GetProperty("scryfall_uri").GetString()!;
+            var imageUri = GetImageUri(cardJson);
+            var set = $"spg-{setCode}";
+            var scryfallId = cardJson.GetProperty("id").GetString()!;
+            return new Card(name, rarity, scryfallId, set, oracleId, scryfallUri, imageUri);
+        }
+        catch (Exception)
+        {
+            throw new ArgumentException($"Could not parse {cardJson.ToString()}");
+        }
+    }
 
     private static string GetImageUri(JsonElement cardJson)
     {
