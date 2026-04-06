0e7c11cb23da6cf189d9c579623e2413e717ab85
Liam Cox <liam@liamc.ca>
2026-04-06 18:56:04 -0400

    Update configuration to include spg pull rates by set

--- src/MTGallery.Configuration/ConfiguredSetsOptions.cs
+++ src/MTGallery.Configuration/ConfiguredSetsOptions.cs
@@ -6,6 +6,7 @@ public class ConfiguredSetsOptions
     public required HashSet<string> ConfiguredCommanderSets { get; init; }
     public required bool HydrateSetData { get; init; }
     public required bool SpecialGuestsEnabled { get; init; }
-    public required Dictionary<string, string> SpecialGuestRangesBySet { get; init; } 
+    public required Dictionary<string, string> SpecialGuestRangesBySet { get; init; }
+    public required Dictionary<string, string> SpecialGuestRatesBySet { get; init; }
     public HashSet<string> AllConfiguredSets => ConfiguredCommanderSets.Concat(ConfiguredSets).ToHashSet();
 }
