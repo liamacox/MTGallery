b6c46f84bce324aec671c002a2787e254e5ca859
Liam Cox <liam@liamc.ca>
2026-04-06 18:32:30 -0400

    remove unecessary configuration: special guest ranges by set implies a set is enabled

--- src/MTGallery.Configuration/ConfiguredSetsOptions.cs
+++ src/MTGallery.Configuration/ConfiguredSetsOptions.cs
@@ -6,7 +6,6 @@ public class ConfiguredSetsOptions
     public required HashSet<string> ConfiguredCommanderSets { get; init; }
     public required bool HydrateSetData { get; init; }
     public required bool SpecialGuestsEnabled { get; init; }
-    public required HashSet<string> SpecialGuestsEnabledSets { get; init; }
     public required Dictionary<string, string> SpecialGuestRangesBySet { get; init; } 
     public HashSet<string> AllConfiguredSets => ConfiguredCommanderSets.Concat(ConfiguredSets).ToHashSet();
 }
