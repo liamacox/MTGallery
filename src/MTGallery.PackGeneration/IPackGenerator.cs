d5f3f1f9f1552f89af95d33eee4a07755f74b87f
Liam Cox <liam@liamc.ca>
2026-04-16 13:16:23 -0400

    Create pack generation coordinator and IPackGenerator

---
+++ src/MTGallery.PackGeneration/IPackGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Frozen;
+using MTGallery.Domain;
+
+namespace MTGallery.PackGeneration;
+
+internal interface IPackGenerator
+{
+    public Task<FrozenDictionary<Card, int>> GeneratePacksAsync(int numberOfPacks = 1);
+}
---
+++ src/MTGallery.PackGeneration/PackGenerationCoordinator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Frozen;
+using MTGallery.Configuration;
+using MTGallery.Domain;
+using MTGallery.Persistence;
+
+namespace MTGallery.PackGeneration;
+
+public class PackGenerationCoordinator(PostgreSqlRepository repository, ConfiguredSetsOptions configuredSetsOptions)
+{
+    private static readonly FrozenDictionary<string, IPackGenerator> _packGenerators = 
+        new Dictionary<string, IPackGenerator>().ToFrozenDictionary();
+    
+    public Task<FrozenDictionary<Card, int>> GeneratePacksAsync(string setCode, int numberOfPacks = 1)
+    {
+        if (!_packGenerators.ContainsKey(setCode))
+            throw new ArgumentException($"{setCode} is not a configured set!");
+        if (numberOfPacks < 1)
+            throw new ArgumentException("Invalid number of packs!");
+
+        return _packGenerators[setCode].GeneratePacksAsync(numberOfPacks);
+    }
+}
