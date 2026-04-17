9798f351c730ef5fecfeab054e840a517099929d
Liam Cox <liam@liamc.ca>
2026-04-16 21:34:23 -0400

    chore: cleanup code and follow coding conventions (#81)
    Closes #80

--- src/MTGallery.Configuration/DatabaseConfigurationOptions.cs
+++ src/MTGallery.Configuration/DatabaseConfigurationOptions.cs
@@ -5,6 +5,6 @@ public class DatabaseConfigurationOptions
     public required string Username { get; init; }
     public required string Password { get; init; }
     public required string Host { get; init; }
-    public required string Port {get; init;}
+    public required string Port { get; init; }
     public required string Database { get; init; }
 }
