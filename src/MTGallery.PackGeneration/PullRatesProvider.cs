9bf4168219bb972561ba2c97999b11282ab5d017
Liam Cox <liam@liamc.ca>
2026-04-16 21:27:29 -0400

    cleanup

--- src/MTGallery.PackGeneration/PullRatesProvider.cs
+++ src/MTGallery.PackGeneration/PullRatesProvider.cs
@@ -7,12 +7,12 @@ namespace MTGallery.PackGeneration;
 internal static class PullRatesProvider
 {
     private const string BlbRates = """
-                                     [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":180,"Uncommon":600,"Rare":210,"Mythic":30},{"Common":0,"Uncommon":0,"Rare":838,"Mythic":153},{"Common":604,"Uncommon":308,"Rare":85,"Mythic":21}]
-                                     """;
+                                    [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":180,"Uncommon":600,"Rare":210,"Mythic":30},{"Common":0,"Uncommon":0,"Rare":838,"Mythic":153},{"Common":604,"Uncommon":308,"Rare":85,"Mythic":21}]
+                                    """;
 
     private const string EclRates = """
-                                     [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":180,"Uncommon":600,"Rare":210,"Mythic":30},{"Common":0,"Uncommon":0,"Rare":838,"Mythic":153},{"Common":604,"Uncommon":308,"Rare":85,"Mythic":21}]
-                                     """;
+                                    [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":180,"Uncommon":600,"Rare":210,"Mythic":30},{"Common":0,"Uncommon":0,"Rare":838,"Mythic":153},{"Common":604,"Uncommon":308,"Rare":85,"Mythic":21}]
+                                    """;
 
     private const string SosRates = """
                                     [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":391,"Uncommon":391,"Rare":200,"Mythic":24},{"Common":0,"Uncommon":0,"Rare":845,"Mythic":154},{"Common":0,"Uncommon":875,"Rare":96,"Mythic":29},{"Common":544,"Uncommon":364,"Rare":77,"Mythic":21}]
@@ -23,7 +23,7 @@ internal static class PullRatesProvider
         {
             { "blb", JsonSerializer.Deserialize<List<PullRates>>(BlbRates)! },
             { "ecl", JsonSerializer.Deserialize<List<PullRates>>(EclRates)! },
-            { "sos", JsonSerializer.Deserialize<List<PullRates>>(SosRates)!}
+            { "sos", JsonSerializer.Deserialize<List<PullRates>>(SosRates)! }
         }.ToFrozenDictionary();
 
     internal static IReadOnlyList<PullRates> GetPullRates(string setCode)
