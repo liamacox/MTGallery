b612577111a32a1c6ad1c460bbc423ee132d0f8b
Liam Cox <liam@liamc.ca>
2026-04-16 21:30:02 -0400

    cleanup

--- src/MTGallery.Domain/Card.cs
+++ src/MTGallery.Domain/Card.cs
@@ -13,13 +13,13 @@ public record Card(
     string ScryfallUri,
     string ImageUri,
     int CollectorNumber)
-{ 
+{
     public static Card BuildCardFromJson(JsonElement cardJson)
     {
         try
         {
             var name = cardJson.GetProperty("name").GetString()!;
-            var rarity = Enum.Parse<Rarity>(cardJson.GetProperty("rarity").GetString()!, ignoreCase: true);
+            var rarity = Enum.Parse<Rarity>(cardJson.GetProperty("rarity").GetString()!, true);
             var oracleId = cardJson.GetProperty("oracle_id").GetString()!;
             var scryfallUri = cardJson.GetProperty("scryfall_uri").GetString()!;
             var imageUri = GetImageUri(cardJson);
@@ -33,13 +33,11 @@ public record Card(
             throw new ArgumentException($"Could not parse {cardJson.ToString()}");
         }
     }
-    
+
     private static string GetImageUri(JsonElement cardJson)
     {
         if (cardJson.TryGetProperty("image_uris", out var imageUris))
-        {
             return imageUris.GetProperty("small").GetString()!;
-        }
 
         return cardJson.GetProperty("card_faces")
             .EnumerateArray()
