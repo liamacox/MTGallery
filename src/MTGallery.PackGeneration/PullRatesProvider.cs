aec739bc8d6437b788dbe66d84eea7591c3422db
Liam Cox <liam@liamc.ca>
2026-04-16 21:17:13 -0400

    add rates to provider

--- src/MTGallery.PackGeneration/PullRatesProvider.cs
+++ src/MTGallery.PackGeneration/PullRatesProvider.cs
@@ -14,11 +14,16 @@ internal static class PullRatesProvider
                                      [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":180,"Uncommon":600,"Rare":210,"Mythic":30},{"Common":0,"Uncommon":0,"Rare":838,"Mythic":153},{"Common":604,"Uncommon":308,"Rare":85,"Mythic":21}]
                                      """;
 
+    private const string SosRates = """
+                                    [{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":1,"Uncommon":0,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":0,"Uncommon":1,"Rare":0,"Mythic":0},{"Common":391,"Uncommon":391,"Rare":200,"Mythic":24},{"Common":0,"Uncommon":0,"Rare":845,"Mythic":154},{"Common":0,"Uncommon":875,"Rare":96,"Mythic":29},{"Common":544,"Uncommon":364,"Rare":77,"Mythic":21}]
+                                    """;
+
     private static readonly FrozenDictionary<string, IReadOnlyList<PullRates>> PullRatesBySetCode
         = new Dictionary<string, IReadOnlyList<PullRates>>
         {
             { "blb", JsonSerializer.Deserialize<List<PullRates>>(BlbRates)! },
-            { "ecl", JsonSerializer.Deserialize<List<PullRates>>(EclRates)! }
+            { "ecl", JsonSerializer.Deserialize<List<PullRates>>(EclRates)! },
+            { "sos", JsonSerializer.Deserialize<List<PullRates>>(SosRates)!}
         }.ToFrozenDictionary();
 
     internal static IReadOnlyList<PullRates> GetPullRates(string setCode)
