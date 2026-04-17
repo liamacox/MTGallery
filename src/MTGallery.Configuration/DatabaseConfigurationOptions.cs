88eed6bd3d56bde7169da78cf240ad20c0084e58
Liam Cox <liam@liamc.ca>
2026-04-16 21:52:42 -0400

    feat: add configured database truncation safety check (#85)
    Closes #84

--- src/MTGallery.Configuration/DatabaseConfigurationOptions.cs
+++ src/MTGallery.Configuration/DatabaseConfigurationOptions.cs
@@ -7,4 +7,5 @@ public class DatabaseConfigurationOptions
     public required string Host { get; init; }
     public required string Port { get; init; }
     public required string Database { get; init; }
+    public required bool AllowTruncation { get; init; }
 }
