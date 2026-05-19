7b36a43c0096c370ca000d7610583f9226cca7f6
Liam Cox <liam@liamc.ca>
2026-05-19 17:18:55 -0400

    Create initial configuration files

---
+++ src/MTGallery.PackGeneration/PackGeneratorConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+using MTGallery.Domain;
+
+namespace MTGallery.PackGeneration;
+
+internal class SetWithMysticalArchiveAndSpecialGuestConfiguration : PackGeneratorConfiguration
+{
+    public required string MysticalArchiveSet { get; init; }
+}
+
+internal class SetWithSpgPackGeneratorConfiguration : PackGeneratorConfiguration;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "SetType")]
+[JsonDerivedType(typeof(SetWithSpgPackGeneratorConfiguration), "SpecialGuest")]
+[JsonDerivedType(typeof(SetWithMysticalArchiveAndSpecialGuestConfiguration), "SpecialGuestAndMysticalArchive")]
+internal abstract class PackGeneratorConfiguration
+{
+    public required string SetCode { get; init; }
+    public required int SpecialGuestRateNumerator { get; init; }
+    public required int SpecialGuestRateDenominator { get; init; }
+    public required int SpecialGuestCollectorNumberLowerBound { get; init; }
+    public required int SpecialGuestCollectorNumberUpperBound { get; init; }
+    public required IReadOnlyList<PullRates> PullRates { get; init; }
+}
+    
