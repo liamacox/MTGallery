8e0ffc35a9560f6ea6a78d5f045558c8b7f6daf7
Liam Cox <liam@liamc.ca>
2026-05-19 17:29:32 -0400

    Create pack generator factory

---
+++ src/MTGallery.PackGeneration/PackGeneratorFactory.cs
@@ -0,0 +1,38 @@
+﻿using MTGallery.PackGeneration.Generators;
+using MTGallery.Persistence;
+
+namespace MTGallery.PackGeneration;
+
+public class PackGeneratorFactory(PostgreSqlRepository repository)
+{
+    IPackGenerator GetPackGenerator(PackGeneratorConfiguration configuration)
+        => configuration switch
+        {
+            SetWithSpgPackGeneratorConfiguration generatorConfiguration => 
+                GetSetWithSpgPackGenerator(generatorConfiguration),
+            SetWithMysticalArchiveAndSpecialGuestConfiguration generatorConfiguration => 
+                GetSetWithMysticalArchiveAndSpecialGuestPackGenerator(generatorConfiguration),
+            _ => throw new ArgumentException("Unsupported configuration!")
+        };
+
+    private IPackGenerator GetSetWithMysticalArchiveAndSpecialGuestPackGenerator(
+        SetWithMysticalArchiveAndSpecialGuestConfiguration configuration)
+        => new SetWithSpgPackGenerator(
+            configuration.SetCode,
+            configuration.SpecialGuestRateNumerator,
+            configuration.SpecialGuestRateDenominator,
+            configuration.SpecialGuestCollectorNumberLowerBound,
+            configuration.SpecialGuestCollectorNumberUpperBound,
+            configuration.PullRates,
+            repository);
+
+    private IPackGenerator GetSetWithSpgPackGenerator(SetWithSpgPackGeneratorConfiguration configuration)
+        => new SetWithSpgPackGenerator(
+            configuration.SetCode,
+            configuration.SpecialGuestRateNumerator,
+            configuration.SpecialGuestRateDenominator,
+            configuration.SpecialGuestCollectorNumberLowerBound,
+            configuration.SpecialGuestCollectorNumberUpperBound,
+            configuration.PullRates,
+            repository);
+}
